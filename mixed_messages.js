7c5e0c009431fe9544093ea8a959e8b4d1aa6588
чарвис <24801250+itssoma@users.noreply.github.com>
2020-12-28 02:41:54 -0800

    Fixed a major sentence structure error on my part :/

--- mixed_messages.js
+++ mixed_messages.js
@@ -9,37 +9,41 @@ var pos = [['Мой','My','m'],['Моя','My','f'],['Его','His','0'],['Её',
 
 //Subject Array
 //first index of the array is russian, the scond Eglish, and the third is the gender of the subject.
-var subj = [['собака','dog','f'],['профессор','professor','m'],['сестра','sister','f'],['брат','brother','m']];
+var subj = [['собака.','dog.','f'],['профессор.','professor.','m'],['сестра.','sister.','f'],['брат.','brother.','m']];
 
 //Adjectives Array the first index is the masculine version, the second 
-var adj = [['хороший.','хорошая.','is good.'],['устал.','устала.','is tired.'],['крепкий.','крепкая.','is tough.'],['толстый.','толстая.','is fat.']];
+var adj = [['хороший','хорошая','good'],['усталый','усталая','tired'],['крепкий','крепкая','tough'],['толстый','толстая','fat']];
 
 // arrays that will capture the output settings.
 var sentence = [];
 sentence.push(pos[Math.floor(Math.random() * pos.length)]);
+sentence.push(adj[Math.floor(Math.random() * subj.length)]);
 sentence.push(subj[Math.floor(Math.random() * subj.length)]);
 
 //while loop to ensure the correct 'My' is used based on the subjects gender.
-while(sentence[1][2] != sentence[0][2] && sentence[0][2] != '0')
+while(sentence[2][2] != sentence[0][2] && sentence[0][2] != '0')
 {
     sentence.pop();
     sentence.push(subj[Math.floor(Math.random() * subj.length)]);
 }
-sentence.push(adj[Math.floor(Math.random() * adj.length)]);
 
 // arrays to store the generated sentence. one for russian and another from english.
-var ru_sentence = [sentence[0][0],sentence[1][0]];
-var eng_sentence = [sentence[0][1],sentence[1][1],sentence[2][2]];
+var ru_sentence = [sentence[0][0]];
+var eng_sentence = [sentence[0][1],sentence[1][2],sentence[2][1]];
 
 // determining which adjective spelling to choose based on the subjects gender type
-if(sentence[1][2] === 'm')
+if(sentence[2][2] === 'm' || sentence[2][2] === '0')
 {
-    ru_sentence.push(sentence[2][0]);
+    ru_sentence.push(sentence[1][0]);
 }
 else
 {
-    ru_sentence.push(sentence[2][1]);
+    ru_sentence.push(sentence[1][1]);
 }
+
+ru_sentence.push(sentence[2][0]);
+
+
 
 // printed console statement.
 console.log("\n\n---------- RANDOM MESSAGE -----------");
